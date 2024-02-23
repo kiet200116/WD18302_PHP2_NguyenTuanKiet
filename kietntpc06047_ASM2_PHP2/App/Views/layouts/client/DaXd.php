84fa5faa463c507b704e726310a3e9f46b424fa2
kiet200116 <kietdragonpro17@gmail.com>
2024-02-23 19:12:48 +0700

    asm2
    asm2

---
+++ kietntpc06047_ASM2_PHP2/App/Views/layouts/client/DaXd.php
@@ -0,0 +1,75 @@
+ <!-- Content Start -->
+ <div class="content">
+ <a href="?url=DaXd/loadViewDaXd" ></a>
+            <!-- Navbar Start -->
+            <nav class="navbar navbar-expand bg-secondary navbar-dark sticky-top px-4 py-0">
+                <a href="index.php" class="navbar-brand d-flex d-lg-none me-4">
+                    <h2 class="text-primary mb-0"><i class="fa fa-user-edit"></i></h2>
+                </a>
+                <a href="#" class="sidebar-toggler flex-shrink-0">
+                    <i class="fa fa-bars"></i>
+                </a>
+                <form class="d-none d-md-flex ms-4">
+                    <input class="form-control bg-dark border-0" type="search" placeholder="Search">
+                </form>
+                <div class="navbar-nav align-items-center ms-auto">
+                    <div class="nav-item dropdown">
+                        
+                    </div>
+                   
+                    <div class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
+                            <img class="rounded-circle me-lg-2" src="../Src/img/user.jpg" alt="" style="width: 40px; height: 40px;">
+                            <span class="d-none d-lg-inline-flex">John Doe</span>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-end bg-secondary border-0 rounded-0 rounded-bottom m-0">
+                            <a href="#" class="dropdown-item">Thông tin cá nhân</a>
+                            <a href="#" class="dropdown-item">Cài đặt</a>
+                            <a href="?url=LoginController/logout" class="dropdown-item">Đăng xuất</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+            <!-- Navbar End -->
+
+            <!-- Recent Sales Start -->
+            <div class="container-fluid pt-4 px-4">
+                <div class="bg-secondary text-center rounded p-4">
+                    <div class="d-flex align-items-center justify-content-between mb-4">
+                        <h6 class="mb-0">Danh sách dự án xây đường</h6>
+                        <a href="">Xem tất cả</a>
+                    </div>
+                    <div class="table-responsive">
+                        <table class="table text-start align-middle table-bordered table-hover mb-0">
+                            <thead>
+                                <tr class="text-white">
+                                <th scope="col">ID</th>
+                                    <th scope="col">Thời gian bắt đầu</th>
+                                    <th scope="col">Thời gian kết thúc</th>
+                                    <th scope="col">Tên dự án</th>
+                                    
+                                    <th scope="col">Họ và tên khách hàng</th>
+                                    
+                                    <th scope="col">Trạng thái</th>
+                                    <th scope="col">Hành động</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                <td>1</td>
+                                    <td>01 Jan 2045</td>
+                                    <td>INV-0123</td>
+                                    <td>Dự án xây đường</td>
+                                    <td>Nguyễn Tuấn Kiệt</td>
+                                    <td>hiện</td>
+                                    <td><a class="btn btn-sm btn-primary" href="">Ẩn</a>
+                                    <a class="btn btn-sm btn-primary" href="">hiện</a></td>
+                                </tr>
+                                <tr>
+                                
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <!-- Recent Sales End -->
---
+++ kietntpc06047_ASM2_PHP2/index.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once "vendor\autoload.php";
+
+
+define ("ROOT_URL", "http://127.0.0.1:5000");
+
+use App\Models\Database;
+use App\Models\UserModel;
+
+// $user = new UserModel();
+// var_dump($user->checkUserExist('tinhpv10@fpt.edu.vn'));
+
+// var_dump(password_hash("123456", 1));
+
+use App\Core\Route;
+
+new Route;
+
+
